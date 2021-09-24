8632acb00698234fb7a9d66cb90cfbeded61eb26
piero <gagonzabe@gmail.com>
2021-09-24 09:54:30 -0500

    logo header

---
+++ script.js
@@ -0,0 +1,28 @@
+const friend1 = 'Alonso';
+const friend2 =  'Daniel';
+const friend3 = 'Braedy';
+
+const friends = ['Alonso', 'Daniel', 'Braedy'];
+console.log(friends)
+
+const years = new Array(2000,2000,2000,2000);
+
+console.log(friends[friends.length-
+    
+
+    
+
+    
+    1]);
+
+let birthday = 2;
+
+const calcAge = function (birthday){
+    return 2021 - birthday;
+}
+
+console.log(years-1)
+
+
+const ages = [calcAge(years[0]), calcAge(years[1], calcAge(years[years.length-1]))]
+console.log(ages)
